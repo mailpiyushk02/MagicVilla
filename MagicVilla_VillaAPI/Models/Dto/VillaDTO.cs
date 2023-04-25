d6175fba22193ef86aec146d3410b9554f7b6116
mailpiyushk02 <mailpiyushk02@gmail.com>
2023-04-25 14:37:17 +0530

    error removed

---
+++ MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_VillaAPI.Models.Dto
+{
+	public class VillaDTO
+	{
+		public int Id { get; set; }
+		[Required]
+		[MaxLength(10)]
+		public string Name { get; set; }
+	}
+}
+
---
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_VillaAPI.Models
+{
+	public class VillaDTO
+	{
+		public int Id { get; set; }
+		
+		public string Name { get; set; }
+		public DateTime CreatedDate { get; set; }
+	}
+}
+
